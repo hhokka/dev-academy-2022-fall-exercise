44b99899b669b621bd87612087d18e33f8724b29
Hans Hokka <hans.hokka@gmail.com>
2022-08-01 10:03:48 +0300

    added journey controller

---
+++ backend/controllers/journeys.js
@@ -0,0 +1,9 @@
+const router = require('express').Router()
+
+const Journey = require('../models/journey')
+
+router.get('/', async (request, response) => {
+  const journeys = await Journey
+    .find({})
+  response.json(journeys)
+})

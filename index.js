bf40cb8e1b13b8e050c6be976641082ac46ef292
Hans Hokka <hans.hokka@gmail.com>
2022-08-01 07:51:13 +0300

    added file reading and printing to console

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello world')
+const fs = require('fs');
+
+try {
+    const data = fs.readFileSync('./data/small-dataset.csv', 'utf8');
+    console.log(data);
+} catch (err) {
+    console.error(err);
+}

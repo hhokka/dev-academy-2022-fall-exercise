eb83da0ae9246da2a1cf31cd986315ada1fe668f
Hans Hokka <hans.hokka@gmail.com>
2022-08-05 09:23:12 +0300

    modified validation, implemented also for bike stations

--- backend/app.js
+++ backend/app.js
@@ -9,8 +9,8 @@ const journeysRouter = require('./controllers/journeys')
 const bikeStationsRouter = require('./controllers/bikeStations')
 const { errorHandler } = require('./utils/middleware')
 const logger = require('./utils/logger')
-//const validator = require('./utils/validator')
-const loader = require('./utils/loader')
+const validator = require('./utils/validator')
+//const loader = require('./utils/loader')
 
 logger.info('connecting to', config.MONGODB_URI)
 
@@ -33,16 +33,21 @@ if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
-
-//validator.runValidation()
-loader.loadJourneys('./data/2021-05.csv')
+const journeyValidated = validator.runJourneyValidation('./data/very-small-dataset-modified.csv')
+//validator.runJourneyValidation('./data/2021-05.csv')
+//validator.runJourneyValidation('./data/2021-06.csv')
+//validator.runJourneyValidation('./data/2021-07.csv')
+//validator.runBikeStationValidation('./data/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat_avoin.csv')
+const bikeStationValidated = validator.runBikeStationValidation('./data/small-bikeStationDataset.csv')
+console.log('journeyValidated:  ', journeyValidated)
+console.log('bikeStationValidated:  ', bikeStationValidated)
+//loader.loadJourneys('./data/2021-05.csv')
 //loader.loadJourneys('./data/2021-06.csv')
 //loader.loadJourneys('./data/2021-07.csv')
 //loader.loadJourneys('./data/small-dataset.csv')
 //loader.loadJourneys('./data/very-small-dataset.csv')
 //loader.loadBikeStations('./data/Helsingin_ja_Espoon_kaupunkipy%C3%B6r%C3%A4asemat_avoin.csv')
 
-//validate.runValidation2()
 app.use(errorHandler)
 
 
--- backend/utils/validator.js
+++ backend/utils/validator.js
@@ -11,22 +11,19 @@ csvlint.on('data', function(data) {
   console.log(data)
 })
  */
-/* const fs = require('fs')
+const fs = require('fs')
 const csv = require('@fast-csv/parse')
-
-
- */
-
+let journeyValidated = ''
 /** csv file
 name,phone,email,country
 john,123a,john@ph.com,PH
 doe,456,doe@us.com
 */
 
-const csv = require('csv-validator')
-const csvFilePath = './data/very-small-dataset.csv'
+/* const csv = require('csv-validator')
 const headers = {
-  Departure: 1,
+  // eslint-disable-next-line no-useless-escape
+  Departure: '^\S+$',
   Return: '',
   'Departure station id': '',
   'Departure station name': '',
@@ -35,44 +32,87 @@ const headers = {
   'Covered distance (m)': '',
   'Duration (sec.)': ''
 }
-
-const runValidation = () => {
+ */
+/* const runValidation = (fileName) => {
   let validated = null
-  csv(csvFilePath, headers)
+  csv(fileName, headers)
     .then(event => {
       console.log('event: ', event)
       validated = 'true'
     })
     .catch(console.error)
   return (validated)
-}
-/* const runValidation = () => {
+} */
+const runJourneyValidation = (fileName) => {
   let validated = 'true'
-  fs.createReadStream('./data/very-small-dataset.csv')
+  fs.createReadStream(fileName)
     .pipe(csv.parse({ headers: true }))
-    .validate(data => typeof data['Departure'] === 'string')
-    .validate(data => typeof data['Return'] === 'string' )
-    .validate(data => typeof data['Departure station id'] === 'string')
-    .validate(data => typeof data['Departure station name'] === 'string')
-    .validate(data => typeof data['Return station id'] === 'string')
-    .validate(data => typeof data['Return station name'] === 'string')
-    .validate(data => typeof data['Covered distance (m)'] === 'string')
-    .validate(data => typeof data['Duration (sec.)'] === 'string')
+    .validate(data => data['Departure'] !== '')
+    .validate(data => data['Return'] !== '')
+    .validate(data => data['Departure station id'] !== '')
+    .validate(data => data['Departure station name'] !== '')
+    .validate(data => data['Return station id'] !== '')
+    .validate(data => data['Return station name'] !== '')
+    .validate(data => data['Covered distance (m)'] !== '')
+    .validate(data => data['Duration (sec.)'] !== '')
     .on('error', error => console.error(error))
     .on('data', row => {
       console.log(`ROW=${JSON.stringify(row)}`)
-      validated = 'valid'
+      if (validated !== 'invalid'){
+        validated = 'valid'
+
+      }
     })
     .on('data-invalid', (row, rowNumber) => console.log(`Invalid [rowNumber=${rowNumber}] [row=${JSON.stringify(row)}]`))
     .on('data-invalid', () => validated = 'invalid')
     .on('end', rowCount => {
       console.log(`Parsed ${rowCount} rows`)
-      console.log('validated: ', validated)
+
+      console.log('Journeys validated: ', validated)
+      journeyValidated = validated
       return validated
+
     })
+
 }
- */
+
+const runBikeStationValidation = (fileName) => {
+  let validated = 'true'
+  fs.createReadStream(fileName)
+    .pipe(csv.parse({ headers: true }))
+    .validate(data => data['FID'] !== '')
+    .validate(data => data['ID'] !== '')
+    .validate(data => data['Name'] !== '')
+    .validate(data => data['Osoite'] !== '')
+    .validate(data => data['Adress'] !== '')
+    .validate(data => data['Kaupunki'] !== '')
+    .validate(data => data['Stad'] !== '')
+    .validate(data => data['Operaattor'] !== '')
+    .validate(data => data['Kapasiteet'] !== '')
+    .validate(data => data['x'] !== '')
+    .validate(data => data['y'] !== '')
+    .on('error', error => console.error(error))
+    .on('data', row => {
+      console.log(`ROW=${JSON.stringify(row)}`)
+      if (validated !== 'invalid'){
+        validated = 'valid'
+
+      }
+    })
+    .on('data-invalid', (row, rowNumber) => console.log(`Invalid [rowNumber=${rowNumber}] [row=${JSON.stringify(row)}]`))
+    .on('data-invalid', () => validated = 'invalid')
+    .on('end', rowCount => {
+      console.log(`Parsed ${rowCount} rows`)
+      console.log('Journeys validated:', journeyValidated)
+      console.log('Bike stations validated: ', validated)
+
+      return validated
+
+    })
+
+}
+
 /* const runValidation = () => {
   console.log('inside validator')
 } */
-module.exports = { runValidation }
+module.exports = { runJourneyValidation, runBikeStationValidation }

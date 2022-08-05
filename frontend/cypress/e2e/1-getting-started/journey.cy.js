f7aa6448fd44a0beb5a930ce6b31b1ba91ba58c1
Hans Hokka <hans.hokka@gmail.com>
2022-08-05 18:02:50 +0300

    modified first cypress test, debugged singlestation.js

--- frontend/cypress/e2e/1-getting-started/journey.cy.js
+++ frontend/cypress/e2e/1-getting-started/journey.cy.js
@@ -1,22 +1,26 @@
+/* global cy */
 /// <reference types="cypress" />
 
 describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+
   })
 
   it('displays two links and data by default', () => {
+    cy.visit('http://localhost:3000/')
     cy.contains('Journey list')
     cy.contains('Station list')
     cy.contains('Departure station: Töölöntulli | Return station: Pasilan asema | Covered distance in km: 1.87 | Duration in min: 10')
   })
   it('displays station list when clicked', () => {
+    cy.visit('http://localhost:3000/')
     cy.get('#stationList').click()
     cy.contains('Departure station: Töölöntulli')
   })
   it('displays single station view when clicked', () => {
-    cy.contains('#62ebbf5d119bb329c9ca255d29c9ca255d').click()
+    cy.visit('http://localhost:3000/')
+    cy.get('#stationList').click()
+    cy.contains('Töölöntulli').click()
     cy.contains('Station name: Töölöntulli')
   })
- 
 })
